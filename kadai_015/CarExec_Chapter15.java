57fe714c8046bc932ef37db51ffc4d26b5cd5edb
mirage-0112 <yorekumoho@gmail.com>
2025-03-05 20:53:13 +0900

    15章課題再提出
    修正しました。

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,38 +1,14 @@
-package kadai_015;    
+package kadai_015;
 
-// 車クラス
 public class CarExec_Chapter15 {
-    // フィールド
-    private int gear  = 1;  // 1速から5速を表す
-    private int speed = 10; // ギアチェン時後の速度を表す
+    public static void main(String[] args) {
+        // インスタンス化（1つのオブジェクトを作成）
+        Car_Chapter15 car = new Car_Chapter15();
 
-    // コンストラクタ（初期化処理）
-    public CarExec_Chapter15(int gear, int speed) {
-        this.gear = gear;
-        this.speed = speed;
-    }
+        // ギアを3に変更
+        car.gearChange(3);
 
-    // ギアを変更するメソッド
-    public void gearChange(int afterGear) {
-        int beforeGear = this.gear; // 変更前のギアを記録
-        this.gear = afterGear;
-
-        // ギアの値に応じた速度の変更
-        switch (afterGear) {
-            case 1 -> this.speed = 10;
-            case 2 -> this.speed = 20;
-            case 3 -> this.speed = 30;
-            case 4 -> this.speed = 40;
-            case 5 -> this.speed = 50;
-            default -> this.speed = 10;
-        }
-
-        // ギア変更メッセージを正しく表示
-        System.out.println("ギア" + beforeGear + "から" + afterGear + "に切り替えました");
-    }
-
-    // 車の速度を表示するメソッド
-    public void run() {
-        System.out.println("速度は時速" + this.speed + "kmです");
+        // 車の状態を表示
+        car.run();
     }
 }
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,14 +1,32 @@
-package kadai_015;
+package kadai_015;    
 
+//車クラス
 public class Car_Chapter15 {
-    public static void main(String[] args) {
-        // インスタンス化（1つのオブジェクトを作成）
-        CarExec_Chapter15 car = new CarExec_Chapter15(1,10);
+	 // フィールド
+	private int   gear  = 1; //1速から5速を表す
+	private int   speed = 10; //ギアチェン時後の速度を表す
+	 
+	
+	//メソッド
+	public void gearChange ( int afterGear ) {
+	
+	System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+	
+		this.gear = afterGear;
+		//ギアの値
+		switch (afterGear) {
+		case 1 -> this.speed = 10;
+		case 2 -> this.speed = 20;
+		case 3 -> this.speed = 30;
+		case 4 -> this.speed = 40;
+		case 5 -> this.speed = 50;
+		default -> this.speed = 10;
+ 	};
+ 	
+  }
+	
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "kmです");
 
-        // ギアを3に変更
-        car.gearChange(3);
-
-        // 車の状態を表示
-        car.run();
-    }
+	}
 }
